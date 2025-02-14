305a0139f44538662e2af4c16186cef1de07a871
Pham Quan <aminh6c.pmq2@gmail.com>
2025-02-14 23:25:33 +0800

    update train hypothesis and kelly

--- utils/trade.py
+++ utils/trade.py
@@ -5,14 +5,38 @@ from backtesting import Backtest, Strategy
 class BackTrader:
     def __init__(self, data, strategy, commission, exclusive_orders):
         self.data = data 
+        self.train_data = self.data['train_data']
+        self.test_data = self.data['test_data']
+
         self.strategy = strategy 
         self.commission = commission
         self.exclusive_orders = exclusive_orders
+        self.strategy.order_size = 0.9999
 
-    def execute(self):
-        bt = Backtest(data=self.data, strategy=self.strategy, commission=self.commission, exclusive_orders=self.exclusive_orders, trade_on_close=True)
+    def hypothesis(self):
+        bt = Backtest(data=self.train_data, strategy=self.strategy, commission=self.commission, exclusive_orders=self.exclusive_orders, trade_on_close=True)
         stats = bt.run()
 
-        self.trades = stats._trades
+        print(f"Sharpe Ratio is {stats.loc['Sharpe Ratio']} compared to criteria of 1.2")
+        print(f"Sortino Ratio is {stats.loc['Sortino Ratio']} compared to criteria of 1.2")
+        print(f"Win Rate is {stats.loc['Win Rate [%]']} compared to criteria of 50")
+        print(f"Max Drawdown is {stats.loc['Max. Drawdown [%]']} compared to criteria of -10")
+        print(f"Max Drawdown duration is {stats.loc['Max. Drawdown Duration']} compared to criteria of 30")
+        print(f"Kelly criterion is {stats.loc['Kelly Criterion']} use half-Kelly for order size")
+
+        print(f'Hello: {self.strategy.order_size}')
+        self.strategy.order_size = stats.loc['Kelly Criterion']/2
+        print(f'World: {self.strategy.order_size}')
+
+        self.train_trades = stats._trades
+        bt.plot()
+        return stats
+
+
+    def execute(self):
+        bt = Backtest(data=self.test_data, strategy=self.strategy, commission=self.commission, exclusive_orders=self.exclusive_orders, trade_on_close=True)
+        stats = bt.run()
+
+        self.test_trades = stats._trades
         bt.plot()
         return stats
