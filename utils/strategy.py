da20dfdee87adfe215fd95f41b728400d29517b6
Pham Quan <aminh6c.pmq2@gmail.com>
2025-02-21 23:35:30 +0800

    update 21 feb progress

--- utils/strategy.py
+++ utils/strategy.py
@@ -5,12 +5,8 @@ from .signals import EMA
 
 class EmaCross(Strategy):
 
-    # short_duration = 10  # Default values, can be overridden
-    # long_duration = 50
-
-    def __init__(self, short_duration, long_duration):
-        self.short_duration = short_duration
-        self.long_duration = long_duration
+    short_duration = 10  # Default values, can be overridden
+    long_duration = 50
 
     def init(self):
         price = self.data.Close
@@ -18,21 +14,31 @@ class EmaCross(Strategy):
         self.ma2 = self.I(EMA, price, self.long_duration)
 
     def next(self):
-        if crossover(self.ma1, self.ma2) and not self.position:
-            self.buy()
+        # if crossover(self.ma2, self.ma1) and not self.position:
+        #     self.sell(size = self.order_size)
+
+        # elif crossover(self.ma1, self.ma2):
+        #     self.position.close()
+
+        # if crossover(self.ma1, self.ma2) and not self.position:
+        #     self.buy(size = self.order_size)
+        # elif crossover(self.ma2, self.ma1):
+        #     self.position.close()
+
+        
+        if crossover(self.ma1, self.ma2):
+            if self.position.is_short:
+                self.position.close()
+            
+            self.buy(size = self.order_size)
+
+                
+        
         elif crossover(self.ma2, self.ma1):
-            self.position.close()
+            if self.position.is_long:
+                self.position.close()
+            
+            self.sell(size = self.order_size)
+            
+                
 
-# class SmaCross(Strategy):
-#     params = (('short_duration', 5), ('long_duration', 10))
-
-#     def __init__(self):
-#         # Initialize your indicators based on the parameters
-#         self.ma1 = self.I(EMA, self.data.Close, self.params.short_duration)
-#         self.ma2 = self.I(EMA, self.data.Close, self.params.long_duration)
-
-#     def next(self):
-#         if crossover(self.ma1, self.ma2):
-#             self.buy()
-#         elif crossover(self.ma2, self.ma1):
-#             self.position.close()
--- utils/trade.py
+++ utils/trade.py
@@ -3,7 +3,7 @@ from backtesting import Backtest, Strategy
 
 
 class BackTrader:
-    def __init__(self, data, strategy, commission, exclusive_orders):
+    def __init__(self, data, strategy, commission, exclusive_orders, plot=False, print=False):
         self.data = data 
         self.train_data = self.data['train_data']
         self.test_data = self.data['test_data']
@@ -12,24 +12,26 @@ class BackTrader:
         self.commission = commission
         self.exclusive_orders = exclusive_orders
         self.strategy.order_size = 0.9999
+        self.plot = plot
+        self.print = print
 
     def hypothesis(self):
         bt = Backtest(data=self.train_data, strategy=self.strategy, commission=self.commission, exclusive_orders=self.exclusive_orders, trade_on_close=True)
         stats = bt.run()
 
-        print(f"Sharpe Ratio is {stats.loc['Sharpe Ratio']} compared to criteria of 1.2")
-        print(f"Sortino Ratio is {stats.loc['Sortino Ratio']} compared to criteria of 1.2")
-        print(f"Win Rate is {stats.loc['Win Rate [%]']} compared to criteria of 50")
-        print(f"Max Drawdown is {stats.loc['Max. Drawdown [%]']} compared to criteria of -10")
-        print(f"Max Drawdown duration is {stats.loc['Max. Drawdown Duration']} compared to criteria of 30")
-        print(f"Kelly criterion is {stats.loc['Kelly Criterion']} use half-Kelly for order size")
+        if self.print:
+            print(f"Sharpe Ratio is {stats.loc['Sharpe Ratio']} compared to criteria of 1.2")
+            print(f"Sortino Ratio is {stats.loc['Sortino Ratio']} compared to criteria of 1.2")
+            print(f"Win Rate is {stats.loc['Win Rate [%]']} compared to criteria of 50")
+            print(f"Max Drawdown is {stats.loc['Max. Drawdown [%]']} compared to criteria of -10")
+            print(f"Max Drawdown duration is {stats.loc['Max. Drawdown Duration']} compared to criteria of 30")
+            print(f"Kelly criterion is {stats.loc['Kelly Criterion']} use half-Kelly for order size")
 
-        print(f'Hello: {self.strategy.order_size}')
-        self.strategy.order_size = stats.loc['Kelly Criterion']/2
-        print(f'World: {self.strategy.order_size}')
+        # self.strategy.order_size = stats.loc['Kelly Criterion']/2
 
         self.train_trades = stats._trades
-        bt.plot()
+        if self.plot:
+            bt.plot()
         return stats
 
 
