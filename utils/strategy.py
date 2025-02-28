5346e768fd61c10b0b8887252e73108c0c6be1fa
Pham Quan <aminh6c.pmq2@gmail.com>
2025-02-28 23:16:54 +0800

    update 28 feb progress

--- utils/strategy.py
+++ utils/strategy.py
@@ -8,37 +8,35 @@ class EmaCross(Strategy):
     short_duration = 10  # Default values, can be overridden
     long_duration = 50
 
+    id = 0
+    take_profit_ratio = 1.1
+    stop_loss_ratio = 0.9
+
     def init(self):
         price = self.data.Close
         self.ma1 = self.I(EMA, price, self.short_duration)
         self.ma2 = self.I(EMA, price, self.long_duration)
 
     def next(self):
+        entry_price = self.data.Close[-1]
         # if crossover(self.ma2, self.ma1) and not self.position:
         #     self.sell(size = self.order_size)
 
         # elif crossover(self.ma1, self.ma2):
         #     self.position.close()
-
-        # if crossover(self.ma1, self.ma2) and not self.position:
-        #     self.buy(size = self.order_size)
-        # elif crossover(self.ma2, self.ma1):
-        #     self.position.close()
-
-        
         if crossover(self.ma1, self.ma2):
-            if self.position.is_short:
-                self.position.close()
-            
-            self.buy(size = self.order_size)
+            self.buy(size = self.order_size, tp=entry_price*self.take_profit_ratio, sl=entry_price*self.stop_loss_ratio, tag=f'Long {self.id}')
+            self.id += 1
 
-                
-        
+        # if len(self.trades) < 3:
+        #     self.buy()
+        # else:
+        #     pass
         elif crossover(self.ma2, self.ma1):
-            if self.position.is_long:
-                self.position.close()
-            
-            self.sell(size = self.order_size)
+            for trade in self.trades:
+                if trade.tag ==f'Long {self.id-1}':
+                    trade.close()
+
             
                 
 
--- utils/trade.py
+++ utils/trade.py
@@ -3,21 +3,23 @@ from backtesting import Backtest, Strategy
 
 
 class BackTrader:
-    def __init__(self, data, strategy, commission, exclusive_orders, plot=False, print=False):
+    def __init__(self, data, strategy, commission, params, plot=False, print=False):
         self.data = data 
         self.train_data = self.data['train_data']
         self.test_data = self.data['test_data']
 
         self.strategy = strategy 
+        self.params = params 
+
         self.commission = commission
-        self.exclusive_orders = exclusive_orders
-        self.strategy.order_size = 0.9999
+        self.strategy.order_size = 0.1
+
         self.plot = plot
         self.print = print
 
     def hypothesis(self):
-        bt = Backtest(data=self.train_data, strategy=self.strategy, commission=self.commission, exclusive_orders=self.exclusive_orders, trade_on_close=True)
-        stats = bt.run()
+        bt = Backtest(data=self.train_data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
+        stats = bt.run(**self.params)
 
         if self.print:
             print(f"Sharpe Ratio is {stats.loc['Sharpe Ratio']} compared to criteria of 1.2")
@@ -36,8 +38,8 @@ class BackTrader:
 
 
     def execute(self):
-        bt = Backtest(data=self.test_data, strategy=self.strategy, commission=self.commission, exclusive_orders=self.exclusive_orders, trade_on_close=True)
-        stats = bt.run()
+        bt = Backtest(data=self.test_data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
+        stats = bt.run(**self.params)
 
         self.test_trades = stats._trades
         bt.plot()
