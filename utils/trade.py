8c223da0ab4ca47320c0e1d79d07ce28d85efe18
Pham Quan <aminh6c.pmq2@gmail.com>
2025-03-02 16:10:09 +0800

    merge execute and add optimize

--- utils/trade.py
+++ utils/trade.py
@@ -3,7 +3,7 @@ from backtesting import Backtest, Strategy
 
 
 class BackTrader:
-    def __init__(self, data, strategy, commission, params, plot=False, print=False):
+    def __init__(self, data, strategy, commission, params, grid_search, plot=False, print=False):
         self.data = data 
         self.train_data = self.data['train_data']
         self.test_data = self.data['test_data']
@@ -14,12 +14,24 @@ class BackTrader:
         self.commission = commission
         self.strategy.order_size = 0.1
 
+        self.trades = dict()
+
         self.plot = plot
         self.print = print
+        self.grid_search = grid_search
 
-    def hypothesis(self):
-        bt = Backtest(data=self.train_data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
+    def execute(self, mode='train', plot=False, print=False):
+        if mode == 'test':
+            plot = True
+            data = self.test_data
+        elif mode == 'train':
+            plot = False 
+            data = self.train_data 
+
+        bt = Backtest(data=data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
         stats = bt.run(**self.params)
+
+        self.trades[mode] = stats._trades
 
         if self.print:
             print(f"Sharpe Ratio is {stats.loc['Sharpe Ratio']} compared to criteria of 1.2")
@@ -28,19 +40,21 @@ class BackTrader:
             print(f"Max Drawdown is {stats.loc['Max. Drawdown [%]']} compared to criteria of -10")
             print(f"Max Drawdown duration is {stats.loc['Max. Drawdown Duration']} compared to criteria of 30")
             print(f"Kelly criterion is {stats.loc['Kelly Criterion']} use half-Kelly for order size")
-
-        # self.strategy.order_size = stats.loc['Kelly Criterion']/2
-
-        self.train_trades = stats._trades
-        if self.plot:
+        
+        if plot:
             bt.plot()
+
         return stats
-
-
-    def execute(self):
-        bt = Backtest(data=self.test_data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
-        stats = bt.run(**self.params)
-
-        self.test_trades = stats._trades
-        bt.plot()
-        return stats
+    
+    def optimize(self):
+        bt = Backtest(data=self.train_data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
+        stats, heatmap, result = bt.optimize(
+            maximize='Equity Final [$]',
+            method='sambo',
+            max_tries=100,
+            random_state=0,
+            return_heatmap=True,
+            return_optimization=True,
+            **self.grid_search)
+        
+        return stats, heatmap, result
