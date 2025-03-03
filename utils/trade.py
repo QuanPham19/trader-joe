979ff1e99e4b8de18b81463a207ce8d8d07e1944
Pham Quan <aminh6c.pmq2@gmail.com>
2025-03-03 11:33:23 +0800

    generalize cross-validation params

--- utils/trade.py
+++ utils/trade.py
@@ -3,21 +3,16 @@ from backtesting import Backtest, Strategy
 
 
 class BackTrader:
-    def __init__(self, data, strategy, commission, plot=False, print=False):
+    def __init__(self, data, strategy, commission):
         self.data = data 
         self.train_data = self.data['train_data']
         self.test_data = self.data['test_data']
 
         self.strategy = strategy 
-
         self.commission = commission
         self.strategy.order_size = 0.1
 
         self.trades = dict()
-
-        self.plot = plot
-        self.print = print
-        # self.grid_search = grid_search
 
     def execute(self, params, data, plot=False, print=False):
         bt = Backtest(data=data, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
@@ -25,7 +20,7 @@ class BackTrader:
 
         self.trades = stats._trades
 
-        if self.print:
+        if print:
             print(f"Sharpe Ratio is {stats.loc['Sharpe Ratio']} compared to criteria of 1.2")
             print(f"Sortino Ratio is {stats.loc['Sortino Ratio']} compared to criteria of 1.2")
             print(f"Win Rate is {stats.loc['Win Rate [%]']} compared to criteria of 50")
@@ -50,10 +45,13 @@ class BackTrader:
 
             bt = Backtest(data=df_train, strategy=self.strategy, commission=self.commission, finalize_trades=True, exclusive_orders=False, trade_on_close=True)
             stats, result = bt.optimize(maximize=metrics, method='sambo', max_tries=100, random_state=0, return_heatmap=False, return_optimization=True, **grid)
-            params = {'short_duration': result['x'][0], 'long_duration': result['x'][1]}
-            print(params) 
 
-            stats = self.execute(params=params, data=df_test)
+            # To be upgraded
+            # params = {'short_duration': result['x'][0], 'long_duration': result['x'][1]}
+            best_params = {key: result.x[i] for i, key in enumerate(grid.keys())}
+            print(best_params) 
+
+            stats = self.execute(params=best_params, data=df_test)
             score = stats.loc[metrics]
             print(score)
 
